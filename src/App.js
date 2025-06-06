dd901f3ae79d551b1fec2a89ccf31bf1bf02744a
Nikola Vuletić <nikolavuletic412@gmail.com>
2025-06-06 02:11:23 +0200

    added delete funcionality, some footer functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
-
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems(items => [...items, item]);
+    // setNumItems((num) => num + 1);
+  }
+
+  function handleDeleteItem(id) {
+    setItems(items=>items.filter(item=>item.id !== id))
+  }
+
+  function handleToggleItem(id) {
+    setItems(items => items.map(item => item.id === id ?{...item, packed : !item.packed} : item)
+  );
+  }
+
   return (
     <div className="app">
     <Logo />
-    <Form />
-    <PackingList />
-    <Stats />
+    <Form onAddItems={handleAddItems} />
+    <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
+    <Stats items={items} />
   </div>
   );
 }
@@ -21,9 +31,12 @@ function Logo() {
   return <h1>🌴 Far Away 👜</h1>
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+  
+
+  
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -33,12 +46,14 @@ function handleSubmit(e) {
   const newItem ={description, quantity, packed: false, id: Date.now() };
   console.log(newItem);
 
+  onAddItems(newItem)
+
   setDescription('');
   setQuantity(1);
 }
 
   return (
-  <form className="add-form" onSubmit={(e) => handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
   <h3>What do you need for your 😍 trip? </h3>
   <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))} >
     {Array.from({length: 20}, (_, i) => i +1).map(num=><option value={num} key={num}>{num}</option>)}
@@ -49,31 +64,42 @@ function handleSubmit(e) {
   );
 }
 
-function PackingList() {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return ( 
   <div className="list">
   <ul>
-    {initialItems.map((item) => (
-  <Item item={item} key={item.id} />
+    {items.map((item) => (
+  <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id} />
 ))}
   </ul>
   </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return <li>
+    <input type='checkbox' value={item.packed} onChange={() => onToggleItem(item.id)} />
     <span style={item.packed ? {textDecoration: 'line-through' } : {}}>
       {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
       </li>
 }
 
-function Stats() {
+function Stats({ items }) {
+  if(!items.length) return <p className="stats"><em>Start adding some items to your packing list 🚀</em></p>
+
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentage = Math.round(numPacked / numItems * 100)
+
   return (
   <footer className="stats">
-     <em>👜 You have X items on your list, and you already packed X (X% vbmn)</em>
+     <em>
+      {percentage === 100 ? 'You got everything! Ready to go ✈' : 
+      `👜 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`
+    }
+      </em>
   </footer>
   );
 }
